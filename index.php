c2ad440efde13023e47338791a278ff98cc80788
nikolabibercic <nikolabibercic@gmail.com>
2021-03-15 22:20:53 +0100

    law office uradjen home page

---
+++ index.php
@@ -0,0 +1,28 @@
+
+<?php include 'partials/main-header.php'; ?>
+
+    <!-- services -->
+    <section class="services container">
+        <article>
+            <i class="fas fa-landmark"></i>
+            <h3>Family low</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum exercitationem nam corrupti provident eum quia veritatis cum dolorum nulla eos eligendi, officia consectetur esse laboriosam vitae sed facere expedita atque!</p>
+        </article>
+        <article>
+            <i class="fas fa-landmark"></i>
+            <h3>Business law</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum exercitationem nam corrupti provident eum quia veritatis cum dolorum nulla eos eligendi, officia consectetur esse laboriosam vitae sed facere expedita atque!</p>
+        </article>
+        <article>
+            <i class="fas fa-landmark"></i>
+            <h3>Criminal low</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum exercitationem nam corrupti provident eum quia veritatis cum dolorum nulla eos eligendi, officia consectetur esse laboriosam vitae sed facere expedita atque!</p>
+        </article>
+        <article>
+            <i class="fas fa-landmark"></i>
+            <h3>Insurance low</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum exercitationem nam corrupti provident eum quia veritatis cum dolorum nulla eos eligendi, officia consectetur esse laboriosam vitae sed facere expedita atque!</p>
+        </article>
+    </section>
+
+<?php include 'partials/footer.php'; ?>
---
+++ partials/main-header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Advokat</title>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="scss/style.css">
+</head>
+<body>
+
+    <!-- topBar -->
+    <section class="topBar container">
+        <a href="index.php" class="logo">Law <span>office</span></a>
+        <ul>
+            <li class=""><a href=""><i class="fab fa-facebook-f"></i></a></li>
+            <li class=""><a href=""><i class="fab fa-twitter"></i></a></li>
+            <li class=""><a href=""><i class="fab fa-instagram"></i></a></li>
+        </ul>  
+    </section>
+
+    <!-- mainHeader -->
+    <section class="mainHeader">
+        <nav class="container">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="blog.php">Blog</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+        <hgroup>
+            <h1>Expirience - trust - result</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita nemo itaque est dolore, provident perferendis quae earum sapiente, molestias quibusdam eum. Dolores officia praesentium voluptate molestias modi sequi culpa repellendus?</p>
+            <button>View more</button>
+        </hgroup>  
+    </section>
